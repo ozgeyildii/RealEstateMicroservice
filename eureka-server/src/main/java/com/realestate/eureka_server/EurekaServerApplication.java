df4687c37c1dd77c165bf239173bcf5b639abfe1
Özge Yıldırım <o.yildirimm2@gmail.com>
2026-02-28 16:27:16 +0300

    Api gateway container is created

--- eureka-server/src/main/java/com/realestate/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/realestate/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eureka_server;
+package com.realestate.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
